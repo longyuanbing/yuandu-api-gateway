be776f50a90d37f495456844ecbfbb339f06a78a
longyuanbing <longyuanbing@lifesense.com>
2017-09-13 17:19:52 +0800

    first commit

---
+++ src/main/java/com/yuandu/App.java
@@ -0,0 +1,24 @@
+package com.yuandu;
+
+import com.yuandu.gateway.filter.AccessSignFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 使用@EnableZuulProxy注解激活zuul
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public AccessSignFilter accessSignFilter() {
+        return new AccessSignFilter();
+    }
+}
